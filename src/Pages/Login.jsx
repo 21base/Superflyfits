db88dc88c18de794651387179ec133ace7b517c3
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2022-07-10 18:38:17 +0100

    Made adjustment to the Login.jsx file for better design

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,37 @@
-import Signin from "../Components/Signin"
+import { useState } from "react";
+import styled from "styled-components";
+import Signin from "../Components/Signin";
+import Signup from "../Components/Signup";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${(props) => props.theme.colors.white};
+  opacity: ${(props) => props.type === "signUp" && "0"};
+`;
 
 const Login = () => {
-  return (
-    <>
-        <Signin />
-    </>
-  )
-}
+  const [slideState, setSlideState] = useState({
+    signInSlide: "slideLeft",
+    signUpSlide: "right",
+  });
 
-export default Login
+  const slide = (para1,para2) => {
+    setSlideState({
+      signInSlide: para1,
+      signUpSlide: para2,
+    });
+  }
+
+  return (
+    <Container>
+      <Signin slideSignUp={slide} slidingState={slideState.signInSlide} />
+      <Signup slideSignIn={slide} slidingState={slideState.signUpSlide} />
+    </Container>
+  );
+};
+
+export default Login;
