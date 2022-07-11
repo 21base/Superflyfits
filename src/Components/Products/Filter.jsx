ca6f77b1ea8499c76caee4d7d0810766c4862811
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2022-07-11 16:04:09 +0100

    Added code for filter

---
+++ src/Components/Products/Filter.jsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  padding: 40px 45px;
+`;
+
+const Wrapper = styled.div`
+  padding: 25px 0;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  margin-bottom: 5px;
+`;
+
+const TitleText = styled.h3`
+  text-transform: capitalize;
+`;
+
+const FilterCat = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const Category = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: ${props => props.justify && 'right'};
+`;
+
+const CatTitle = styled.p`
+  text-transform: capitalize;
+  font-weight: 500;
+  margin-right: 15px;
+`;
+
+const Categories = styled.select`
+  text-transform: capitalize;
+  font-weight: 500;
+  background: ${props => props.theme.colors.lighter_grey};
+  width: 100px;
+  width: ${props => props.ex && '130px'};
+  padding: 10px 0;
+  text-align: center;
+  border-radius: 4px;
+  cursor: pointer;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+`;
+
+const CatOptions = styled.option`
+  text-transform: capitalize;
+`;
+
+const Filter = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>
+          <TitleText>filter</TitleText>
+        </Title>
+        <FilterCat>
+          <Category>
+            <CatTitle>categories:</CatTitle>
+            <Categories>
+              <CatOptions>all</CatOptions>
+              <CatOptions>pullover</CatOptions>
+              <CatOptions>sweatshirt</CatOptions>
+              <CatOptions>jogger</CatOptions>
+              <CatOptions>t-shirt</CatOptions>
+              <CatOptions>short</CatOptions>
+            </Categories>
+          </Category>
+          <Category justify={true}>
+            <CatTitle>price:</CatTitle>
+            <Categories ex={true}>
+              <CatOptions>ascending</CatOptions>
+              <CatOptions>descending</CatOptions>
+            </Categories>
+          </Category>
+        </FilterCat>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Filter;
