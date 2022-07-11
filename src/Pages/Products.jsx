56facc8118180692e087bdfd0f6cad5e96c48441
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2022-07-11 16:05:19 +0100

    Added footer and filter components

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,11 +1,17 @@
+import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
+import Filter from "../Components/Products/Filter";
 import HeroSec from "../Components/Products/HeroSec";
+import Shop from "../Components/Products/Shop";
 
 const Products = () => {
   return (
     <>
       <Navbar />
       <HeroSec />
+      <Filter />
+      <Shop />
+      <Footer />
     </>
   );
 };
