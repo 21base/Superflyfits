52a693afbf358d7a7f8a934472a8ccbe99d65ae3
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2022-07-11 01:25:21 +0100

    Added products page to the pages folder

---
+++ src/Pages/Products.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Components/Navbar";
+import HeroSec from "../Components/Products/HeroSec";
+
+const Products = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSec />
+    </>
+  );
+};
+
+export default Products;
